a57acb25125d76d528fd887a0ecc95a521163a77
Mich Ochieng <michochieng@gmail.com>
2022-03-11 14:31:58 -0800

    WeirdList example

--- traverse.hs
+++ traverse.hs
@@ -1,12 +1,16 @@
 {-
 Traversables
+instance Traversable Maybe where
+     traverse f Nothing = pure Nothing
+     traverse f (Just x) = Just <*> f x
 
+f: a -> h b where h is an Applicative
+(Just x): some traversable
+Just <*> f x: h(t b)
+    Also equal to: fmap Just (f x)
 -}
-
--- instance Traversable Maybe where
---     traverse f Nothing = pure Nothing
---     traverse f (Just x) = Just <*> f x
-
--- f: a -> h b where h is an Applicative
--- (Just x): some traversable
--- Just <*> f x: h(t b)
+ 
+-- instance Traversable WeirdList where
+--     traverse f (Single x) = Single <*> f x
+--     traverse f (Pair x y) = Pair <*> f x <*> f y
+--     traverse f (Many xs)  = Mnay <*> traverse (traverse f ) xs
