52564fc20e2cda483c4dd7b7773219f2ea657b3a
Mich Ochieng <michochieng@gmail.com>
2022-03-12 15:49:31 -0800

    Added class

--- monoids.hs
+++ monoids.hs
@@ -1,4 +1,10 @@
 {-
 Monoids
-
+    Associative binary function and a value which acts as an identity with respect to that function
 -}
+
+class Monoid m where
+    mempty  :: m
+    mappend :: m -> m -> m
+    mconcat :: [m] -> m
+
