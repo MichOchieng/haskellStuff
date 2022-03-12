a0abf444334fcc51c64c1f540d6b9b7af5ae4460
Mich Ochieng <michochieng@gmail.com>
2022-03-11 18:38:24 -0800

    Added IO, ziplist and []

--- applicatives.hs
+++ applicatives.hs
@@ -1,4 +1,5 @@
 import Prelude hiding (Applicative)
+import Control.Applicative (ZipList (ZipList))
 {-
 Applicatives
     Allows us to map functions inside of functors over other functors
@@ -13,9 +14,34 @@ class (Functor f) => Applicative f where
 
 instance Applicative Maybe where
     pure          = Just
-    Nothing <*> _ = Nothing    
+    Nothing <*> _ = Nothing
     (Just f ) <*> something = fmap f something
 
--- Test1
+instance Applicative [] where
+    pure x = [x]
+    fs <*> xs = [f x | f <- fs, x <- xs]
+
+instance Applicative IO where
+    pure = return
+    a <*> b = do
+        f <- a
+        f <$> b
+
+instance Applicative ZipList where
+    pure x = ZipList (repeat x)
+    ZipList fs <*> ZipList xs = ZipList (zipWith (\f x -> f x) fs xs)
+
+con :: IO String
+con = (++) <$> getLine Prelude.<*> getLine 
+{-
+    (1) partially applies ++ to the first getLine
+    (2) map the partial function application to the second getLine
+    (3) return the concat' of the two getLines
+-}
+
 main = do
     print $ (++) <$> Just "Happy " Prelude.<*> Just "Birthday"
+    let funcs = [(+2),(*2)]
+    let lst   = [1,2]
+    print $ ZipList funcs Prelude.<*> ZipList lst
+
