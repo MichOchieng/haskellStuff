101a4a6de6b5002d43b7bf29b77e0136221509a6
Mich Ochieng <michochieng@gmail.com>
2022-03-14 00:30:11 -0700

    Added length and map

--- foldable.hs
+++ foldable.hs
@@ -2,3 +2,15 @@
 Foldables
 
 -}
+
+length :: [Int] -> Int
+length = foldr (\x -> (+) 1) 0
+
+myMap :: (a -> a) -> [a] -> [a]
+myMap f = foldr((:) . f) []
+
+
+main = do
+    print $ foldr (\x y -> (x * y) + x) 1 [1,2,3,4]
+    let lst = [1,2,3]
+    print $ myMap (+1) lst
