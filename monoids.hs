e6b2e5c626f8cb326b727fd50a7cdc83f12590e5
Mich Ochieng <michochieng@gmail.com>
2022-03-13 19:09:23 -0700

    Added First instance, newTypes, test and Tree

--- monoids.hs
+++ monoids.hs
@@ -1,10 +1,45 @@
 {-
 Monoids
     Associative binary function and a value which acts as an identity with respect to that function
+
+    (1) Function takes two params
+    (2) Params and the returned val have the same type
+    (3) There exists such a val that doesnt change other values when used with the binary function
 -}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE UndecidableInstances #-}
 
 class Monoid m where
     mempty  :: m
     mappend :: m -> m -> m
     mconcat :: [m] -> m
+
+data Tree a = EmptyTree 
+    | Node a (Tree a) (Tree a)
+    deriving (Show)
+
+newtype Product a = Product {getProduct :: a}
+    deriving (Show,Eq,Read,Bounded,Ord)
+
+newtype First a = First {getFirst :: Maybe a}
+    deriving (Eq,Ord,Read,Show)
+
+instance Semigroup (First a) => Prelude.Monoid (First a) where
+    mempty = First Nothing
+    First (Just x) `mappend` _ = First (Just x)
+    First Nothing `mappend` x  = x
+
+-- instance Monoid a => Monoid (Maybe a) where
+--     mempty = Nothing 
+--     Nothing `mappend` m = m
+--     m `mappend` Nothing = m
+--     Just m1 `mappend` Just m2 = Just (m1 `mappend` m2)
+
+test :: (First a -> Maybe a) -> First a -> Maybe a
+test f (First a) = f $ First a
+
+main = do
+    print $ getFirst $ First (Just 13)
+    print $ "im a " `Prelude.mappend` "monoid"
+
 
