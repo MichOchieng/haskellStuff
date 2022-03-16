8fc2e9a93af97a28e5726cd6546072165855cc28
Mich Ochieng <michochieng@gmail.com>
2022-03-15 18:08:49 -0700

    Added First two exercises

--- foldable.hs
+++ foldable.hs
@@ -1,6 +1,9 @@
 {-
 Foldables
 
+    foldr :: (\elem acc -> term) acc0 list
+    foldl :: (\acc elem -> term) acc0 list
+
 -}
 
 length :: [Int] -> Int
@@ -9,6 +12,28 @@ length = foldr (\x -> (+) 1) 0
 myMap :: (a -> a) -> [a] -> [a]
 myMap f = foldr((:) . f) []
 
+{-
+Exercises
+    (1)
+        Reverse list with one fold
+    (2)
+        Return all prefixes of a given list
+    (3)
+
+    (4)
+
+-}
+
+rev :: [a] -> [a]
+rev = foldl (\acc x -> x:acc) []
+-- f could also be `flip (:)` but this isn't as clear
+
+prefixes :: [a] -> [[a]]
+prefixes = foldr (\x acc -> [x] : (map ((:) x) acc)) []
+
+-- prefixHelper :: [a] -> [a]
+-- prefixHelper []     = []
+-- prefixHelper (x:xs) = prefixHelper $ init (x:xs)
 
 main = do
     print $ foldr (\x y -> (x * y) + x) 1 [1,2,3,4]
