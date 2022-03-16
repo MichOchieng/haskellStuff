cc4cdb4c8cbce9c9e084ef2566f2f8b7a328fac2
Mich Ochieng <michochieng@gmail.com>
2022-03-16 01:48:40 -0700

    first commit

---
+++ continuations.hs
@@ -0,0 +1,31 @@
+{-
+CPS
+    Procedures aren't allowed to return to the caller
+        - Can use callback functons provided by the caller
+
+-}
+
+-- Ex 1
+
+-- Insert a number into a list
+-- If list empty insert into list
+-- If head of list less than incoming num prepend
+--  otherwise append
+insert :: [Int] -> Int -> [Int]
+insert [] y     = [y]
+insert (x:xs) y = 
+    if x > y
+        then y:x:xs
+        else x:insert xs y
+
+cpsInsert :: [Int] -> Int -> ([Int] -> r) -> r
+cpsInsert [] y k = k [y]
+cpsInsert (x:xs) y k = 
+    if x > y
+        then k (y:x:xs)
+        else cpsInsert xs y $ \res -> k (x:res)
+
+-- Ex 2 Length
+
+lstLength :: [a] -> Int 
+lstLength = foldr (\x acc -> acc + 1) 0 
