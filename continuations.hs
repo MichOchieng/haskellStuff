58852bce76359aa57ae90512b977722333abe7b0
Mich Ochieng <michochieng@gmail.com>
2022-04-18 01:29:53 -0700

    Added add

--- continuations.hs
+++ continuations.hs
@@ -13,26 +13,32 @@ CPS
 --  otherwise append
 insert :: [Int] -> Int -> [Int]
 insert [] y     = [y]
-insert (x:xs) y = 
+insert (x:xs) y =
     if x > y
         then y:x:xs
         else x:insert xs y
 
 cpsInsert :: [Int] -> Int -> ([Int] -> r) -> r
 cpsInsert [] y k = k [y]
-cpsInsert (x:xs) y k = 
+cpsInsert (x:xs) y k =
     if x > y
         then k (y:x:xs)
         else cpsInsert xs y $ \res -> k (x:res)
 
 -- Ex 2 Length
 
-lstLength :: [a] -> Int 
-lstLength = foldr (\x acc -> acc + 1) 0 
+lstLength :: [a] -> Int
+lstLength = foldr (\x acc -> acc + 1) 0
 
 cpsLength :: [a] -> Int -> Int
-cpsLength [] acc      = acc 
+cpsLength [] acc      = acc
 cpsLength (x:xs) acc  = cpsLength xs (acc + 1)
+
+add :: Int -> Int -> Int
+add x y = x + y
+
+addC :: Int -> Int -> (Int -> r) -> r
+addC x y k = k (add x y)
 
 myTake :: Int -> [a] -> [a]
 myTake 0 _  = []
@@ -40,6 +46,6 @@ myTake _ [] = []
 myTake i (x:xs) = x : myTake (i-1) xs
 
 
-main = do 
+main = do
     let test = myTake 3 [1..13]
     print test
